4d0f0a98c08c70507a721fa147b6ca1b0baf4b80
lexxgleboff <64305994+lexxgleboff@users.noreply.github.com>
2022-04-05 12:36:28 +0300

    Добавил компоненты ChannelSearch и TeamChannelList

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
-// import Cookies from 'universal-cookie';
-
+import Cookies from 'universal-cookie';
 import { ChannelContainer, ChannelListContainer } from './components';
+
+import './App.css';
 
 const apiKey = 'dsftszhnugs3';
 
@@ -14,9 +15,6 @@ const App = () => {
     <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
             <ChannelListContainer
-
-            />
-            <ChannelContainer
 
             />
         </Chat>
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import chatIcon from '../assets/chatIcon.png'
+import LogoutIcon from '../assets/logout.png'
+import ChannelContainer from './ChannelContainer';
+
+const SideBar = () => (
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className='icon1__inner'>
+                <img src={chatIcon} alt="chatIcon" width="30" />
+            </div>
+        </div>
+        <div className='channel-list__sidebar__icon2'>
+            <div className='icon1__inner'>
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+);
+
+const CompanyHeader = () => (
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>Social Pager Chat</p>
+    </div>
+)
 
 const ChannelListContainer = () => {
   return (
-    <div>
-      ChannelListContainer
-    </div>
+    <>
+          <SideBar />
+          <div className='channel-list__list__wrapper'>
+              <CompanyHeader />
+              <ChannelSearch />
+              <ChannelList 
+                  filters={{}}
+                  channelRenderFilterFn={() => {}}
+                  List={(listProps) => (
+                    <TeamChannelList 
+                          {...listProps}  
+                          type='team'
+                    />
+                  )}
+              />
+          </div>
+          <ChannelContainer />
+          
+    </>
   )
 }
 
